c8bb2ec76529d994def5a98dfdef85ed36ddb5f7
Jan Krüger <jan.krueger@gmail.com>
2012-09-16 16:03:46 +0200

    Added editor presenter implementation

---
+++ cljs/test/webgl/test/presenters/editor.cljs
@@ -0,0 +1,79 @@
+(ns webgl.test.presenters.editor
+  (:require [webgl.kit.rx            :as rx]
+            [webgl.models.operators  :as ops]
+            [webgl.presenters.editor :as ed]
+            [webgl.views.tree        :as tree])
+  (:require-macros [webgl.jasmine       :as j]
+                   [webgl.kit.rx.macros :as rxm]))
+
+(defrecord Mock [events])
+
+(defn mock [& events]
+  (Mock. (apply rx/named-channels events)))
+
+(j/describe "Reloading the model"
+  (j/it "will reset the tree view"
+    (let [reset-spy  (js/spyOn webgl.views.tree "set_root_BANG_")
+          mock-view  (mock tree/node-clicked :keydown)
+          mock-model (mock ops/reload)
+          editor     (ed/present mock-model mock-view)]
+      ;; generate click event
+      (rx/named-event (:events mock-model) ops/reload ::root)
+      ;; the node should have been raised
+      (j/expect reset-spy => (toHaveBeenCalledWith mock-view ::root)))))
+
+(j/describe "Selecting an operator"
+            
+  (j/it "will raise the operator in the tree view"
+    (let [raise-spy (js/spyOn webgl.views.tree "raise")
+          mock-view (mock tree/node-clicked :keydown)
+          editor    (ed/present (mock ops/reload) mock-view)]
+      ;; generate click event
+      (rx/named-event (:events mock-view) tree/node-clicked ::node)
+      ;; the node should have been raised
+      (j/expect raise-spy => (toHaveBeenCalledWith mock-view ::node))))
+  
+  (j/it "will do nothing when clicking the same operator twice"
+    (let [raise-spy (js/spyOn webgl.views.tree "raise")
+          mock-view (mock tree/node-clicked :keydown)
+          editor    (ed/present (mock ops/reload) mock-view)]
+      ;; generate click event
+      (rx/named-event (:events mock-view) tree/node-clicked ::node)
+      (rx/named-event (:events mock-view) tree/node-clicked ::node)
+      ;; the node should have been raised
+      (j/expect (.-callCount raise-spy) => (toEqual 1))))
+  
+  (j/it "will lower the previous selection when clicking another operator"
+    (let [raise-spy (js/spyOn webgl.views.tree "raise")
+          lower-spy (js/spyOn webgl.views.tree "lower")
+          mock-view (mock tree/node-clicked :keydown)
+          editor    (ed/present (mock ops/reload) mock-view)]
+      ;; generate click event
+      (rx/named-event (:events mock-view) tree/node-clicked ::node)
+      (rx/named-event (:events mock-view) tree/node-clicked ::another-node)
+      ;; the node should have been raised
+      (j/expect lower-spy => (toHaveBeenCalledWith mock-view ::node))))
+
+  (j/it "will raise the new operator when switching selections"
+    (let [raise-spy (js/spyOn webgl.views.tree "raise")
+          lower-spy (js/spyOn webgl.views.tree "lower")
+          mock-view (mock tree/node-clicked :keydown)
+          editor    (ed/present (mock ops/reload) mock-view)]
+      ;; generate click event
+      (rx/named-event (:events mock-view) tree/node-clicked ::node)
+      (rx/named-event (:events mock-view) tree/node-clicked ::another-node)
+      ;; the node should have been raised
+      (j/expect raise-spy => (toHaveBeenCalledWith mock-view ::node)))))
+
+(j/describe "Display of an operator"
+  (j/it "is requested by pressing 'r' when an operator is selected"
+    (let [display-spy (js/jasmine.createSpy)
+          mock-view   (mock tree/node-clicked :keydown)
+          editor      (ed/present (mock ops/reload) mock-view)]
+      ;; observe display event
+      (rxm/on (:events editor) ed/display display-spy)
+      ;; select node and generate key event
+      (rx/named-event (:events mock-view) tree/node-clicked ::node)
+      (rx/named-event (:events mock-view) :keydown 82)
+      ;; the selected node should have been display
+      (j/expect display-spy => (toHaveBeenCalledWith ::node)))))
--- cljs/webgl/presenters/editor.cljs
+++ cljs/webgl/presenters/editor.cljs
@@ -1,5 +1,51 @@
 (ns webgl.presenters.editor
-  (:require [webgl.views.tree :as tree]))
+  (:require [webgl.models.operators     :as ops]
+            [webgl.models.selection     :as sel-mod]
+            [webgl.presenters.selection :as sel]
+            [webgl.views.tree           :as tree]
+            [webgl.kit.behaviours       :as b]
+            [webgl.kit.rx               :as rx])
+  (:require-macros [webgl.kit.rx.macros :as rxm]))
+
+(def display ::display)
+
+(defrecord Presenter [events])
+
+(defn- key? [s k]
+  (rx/filter s #(= % k)))
+
+(defn- selected? [s selection]
+  (rx/when s #(sel-mod/selected? selection selection)))
+
+(defn- to-selection [s selection]
+  (-> (selected? s selection)
+      (rx/map #(sel-mod/current selection))))
+
+(defn- fire-display [selection events]
+  (-> (rx/pipe)
+      (key? 82)
+      (to-selection selection)
+      (rx/fire events display)))
+
+(defn- node-selection [view model]
+  (sel/present model view
+    :on-select   tree/raise
+    :on-deselect tree/lower))
+
+(defn- register-view-events [view model events]
+  (let [selection-model (atom nil)]
+    (rxm/on (:events view)
+      tree/node-clicked (node-selection view selection-model)
+      :keydown          (fire-display selection-model events))))
+
+(defn- register-model-events [model view]
+  (rxm/on (:events model)
+    ops/reload #(tree/set-root! view %)))
 
 (defn present [model view]
-  )
+  (let [events (rx/named-channels ::display)]
+    (register-model-events model view)
+    (doto view
+      (b/keep-resized)
+      (register-view-events model events))
+    (Presenter. events)))

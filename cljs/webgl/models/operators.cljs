ca40ba63af523abd263969214fe05b4627c45579
Jan Krüger <jan.krueger@gmail.com>
2012-10-03 15:04:43 +0200

    Implemented an ad hoc model that can update nodes in the model by index

--- cljs/webgl/models/operators.cljs
+++ cljs/webgl/models/operators.cljs
@@ -1,27 +1,58 @@
 (ns webgl.models.operators
-  (:require [webgl.kit.rx           :as rx]
-            [webgl.kit.rx.protocols :as rxp]))
+  (:require [webgl.kit.rx                    :as rx]
+            [webgl.kit.rx.protocols          :as rxp]
+            [webgl.models.operators.factory  :as f]
+            [webgl.models.operators.geometry :as geo]
+            [webgl.models.operators.scalar   :as scalar]))
 
 (def reload ::reload)
-
-(deftype Operator [name children])
+(def update ::update)
 
 (defrecord Model [root events])
 
 (defn make []
   (Model.
-    #{}
-    (rx/named-channels reload)))
+    nil
+    (rx/named-channels reload update)))
 
-(defprotocol Load
-  (load [operator-source]))
-
-(extend-protocol Load
-  Operator
-  (load [root] root))
-
-(defn set! [model operator-source]
-  (let [root (load operator-source)]
-    (set! (.-root model) root)
-    (rx/named-event (:events model) reload root))
+(defn set! [model root]
+  (set! (.-root model) root)
+  (rx/named-event (:events model) reload root)
   nil)
+
+;;; operator API
+
+(defprotocol Operators
+  (op-name     [_])
+  (label       [_])
+  (result-type [_])
+  (operator    [_])
+  (children    [_])
+  (set-input   [_ n child]))
+
+(def Operator f/Operator)
+
+(extend-protocol Operators
+  f/Operator
+  (op-name [op]
+    (:name op))
+  (label [op]
+    (:label op))
+  (result-type [op]
+    (:result-type op))
+  (operator [op]
+    (:operator-fn op))
+  (children [op]
+    @(:inputs op))
+  (set-input [op n child]
+    (swap! (:inputs op) assoc n child)))
+
+(defn eval [op]
+  (apply (operator op) (map eval (children op))))
+
+(defn fire-update [model parent]
+  (rx/named-event (:events model) update parent))
+
+(defn set-child [model op n child]
+  (set-input op n child)
+  (fire-update model op))

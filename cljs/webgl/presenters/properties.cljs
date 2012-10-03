42b53a7562e0219eead959dda9d11637a2f1892b
Jan Krüger <jan.krueger@gmail.com>
2012-10-03 15:07:36 +0200

    Added a presenter for operator properites
    This presenter lets the user edit constant parameters of operators.

---
+++ cljs/webgl/presenters/properties.cljs
@@ -0,0 +1,53 @@
+(ns webgl.presenters.properties
+  (:require [webgl.kit.rx                   :as rx]
+            [webgl.models.operators         :as model]
+            [webgl.models.operators.factory :as model.factory]
+            [webgl.views.form               :as form])
+  (:require-macros [webgl.kit.rx.macros :as rxm]))
+
+(extend-protocol form/Field
+  model/Operator
+  (field-label [op] (model/label op))
+  (field-type  [op] "number")
+  (field-value [op] ((model/operator op))))
+
+(defrecord Presenter [view operator])
+
+(defn- clone [operator value]
+  (model.factory/make
+    :constant
+    (model/result-type operator)
+    value
+    (model/label operator)))
+
+(defn- change-constant [presenter model]
+  (fn [evt]
+    (let [parent   @(:operator presenter)
+          operator (:data evt)
+          index    (:index operator)]
+      (model/set-child model
+        parent
+        index
+        (clone operator (:value evt))))))
+
+(defn register-view-events [presenter view model]
+  (rxm/on (:events view)
+    form/field-changed (change-constant presenter model)))
+
+(defn present [model view]
+  (let [presenter (Presenter. view (atom nil))]
+    (register-view-events presenter view model)
+    presenter))
+
+(defn- property? [op]
+  (= (model/op-name op) :constant))
+
+(defn- ->properties [operator]
+  (->> operator
+       (model/children)
+       (map-indexed #(assoc %2 :index %1))
+       (filter property?)))
+
+(defn show-operator [presenter operator]
+  (reset! (:operator presenter) operator)
+  (form/set! (:view presenter) (->properties operator)))

5f428df3c4bc3689007ea7922921bb7c81f2463a
Jan Krüger <jan.krueger@gmail.com>
2012-11-25 17:01:52 +0100

    Fixed unit tests

--- cljs-test/webgl/test/presenters/editor.cljs
+++ cljs-test/webgl/test/presenters/editor.cljs
@@ -1,7 +1,6 @@
 (ns webgl.test.presenters.editor
   (:require [webgl.kit.rx            :as rx]
             [webgl.models.operators  :as ops]
-            [webgl.models.menu       :as men]
             [webgl.presenters.editor :as ed]
             [webgl.views.tree        :as tree])
   (:require-macros [webgl.jasmine       :as j]
@@ -12,14 +11,12 @@
 (defn mock [& events]
   (Mock. (apply rx/named-channels events)))
 
-(def mock-menu (mock men/reload))
-
 (j/describe "Reloading the model"
   (j/it "will reset the tree view"
     (let [reset-spy  (js/spyOn webgl.views.tree "set_root_BANG_")
           mock-view  (mock tree/node-clicked :keydown)
-          mock-model (mock ops/reload)
-          editor     (ed/present mock-model mock-menu mock-view)]
+          mock-model (mock ops/reload ops/update ops/create)
+          editor     (ed/present mock-model mock-view)]
       ;; generate click event
       (rx/named-event (:events mock-model) ops/reload ::root)
       ;; the node should have been raised
@@ -29,10 +26,8 @@
             
   (j/it "will raise the operator in the tree view"
     (let [raise-spy  (js/spyOn webgl.views.tree "raise")
-          menu-spy   (js/spyOn webgl.presenters.editor "operator__GT_menu")
-          menu2-spy  (js/spyOn webgl.models.menu "set_BANG_")
           mock-view  (mock tree/node-clicked :keydown)
-          editor     (ed/present (mock ops/reload) mock-menu mock-view)]
+          editor     (ed/present (mock ops/reload ops/update ops/create) mock-view)]
       ;; generate click event
       (rx/named-event (:events mock-view) tree/node-clicked ::node)
       ;; the node should have been raised
@@ -40,10 +35,8 @@
   
   (j/it "will do nothing when clicking the same operator twice"
     (let [raise-spy (js/spyOn webgl.views.tree "raise")
-          menu-spy  (js/spyOn webgl.presenters.editor "operator__GT_menu")
-          menu2-spy (js/spyOn webgl.models.menu "set_BANG_")
           mock-view (mock tree/node-clicked :keydown)
-          editor    (ed/present (mock ops/reload) mock-menu mock-view)]
+          editor    (ed/present (mock ops/reload ops/update ops/create) mock-view)]
       ;; generate click event
       (rx/named-event (:events mock-view) tree/node-clicked ::node)
       (rx/named-event (:events mock-view) tree/node-clicked ::node)
@@ -53,10 +46,8 @@
   (j/it "will lower the previous selection when clicking another operator"
     (let [raise-spy (js/spyOn webgl.views.tree "raise")
           lower-spy (js/spyOn webgl.views.tree "lower")
-          menu-spy  (js/spyOn webgl.presenters.editor "operator__GT_menu")
-          menu2-spy (js/spyOn webgl.models.menu "set_BANG_")
           mock-view (mock tree/node-clicked :keydown)
-          editor    (ed/present (mock ops/reload) mock-menu mock-view)]
+          editor    (ed/present (mock ops/reload ops/update ops/create) mock-view)]
       ;; generate click event
       (rx/named-event (:events mock-view) tree/node-clicked ::node)
       (rx/named-event (:events mock-view) tree/node-clicked ::another-node)
@@ -66,10 +57,8 @@
   (j/it "will raise the new operator when switching selections"
     (let [raise-spy (js/spyOn webgl.views.tree "raise")
           lower-spy (js/spyOn webgl.views.tree "lower")
-          menu-spy  (js/spyOn webgl.presenters.editor "operator__GT_menu")
-          menu2-spy (js/spyOn webgl.models.menu "set_BANG_")
           mock-view (mock tree/node-clicked :keydown)
-          editor    (ed/present (mock ops/reload) mock-menu mock-view)]
+          editor    (ed/present (mock ops/reload ops/update ops/create) mock-view)]
       ;; generate click event
       (rx/named-event (:events mock-view) tree/node-clicked ::node)
       (rx/named-event (:events mock-view) tree/node-clicked ::another-node)
